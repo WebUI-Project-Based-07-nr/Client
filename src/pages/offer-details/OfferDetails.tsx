7a87b24e292523e5421981eb974c6006d797ce83
marek777-lol <markgol000@gmail.com>
2024-08-01 11:52:57 +0200

    fixed margin + done square version on mobile

--- src/pages/offer-details/OfferDetails.tsx
+++ src/pages/offer-details/OfferDetails.tsx
@@ -9,25 +9,33 @@ import {
   Button,
   Box,
   Stack,
-  Rating
+  Rating,
 } from '@mui/material'
-
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder'
 
 import temp from './temp.svg'
 import LanguageIcon from '@mui/icons-material/Language'
+import StarIcon from '@mui/icons-material/Star';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder'
+
 import { styles } from '~/pages/offer-details/OfferDetails.styles'
 
 const OfferDetails = () => {
   return (
     <Card sx={styles.card}>
       <CardHeader
+        sx={ styles.cardHeader }
         avatar={<Avatar alt='User' src={temp} sx={styles.avatarImg} />}
         subheader={
           <Box sx={styles.avatarContainer}>
             <Typography sx={styles.name} variant='body2'>
               Jennifer W.
             </Typography>
+            <Box sx={styles.languageContainerPhone}>
+              <LanguageIcon fontSize='small' />
+              <Typography sx={{ ml: 1 }} variant='body2'>
+                Ukrainian, English
+              </Typography>
+            </Box>
             <Box sx={styles.ratingContainer}>
               <Box sx={styles.rating}>
                 <Rating precision={0.5} readOnly size='small' value={3.5} />
@@ -41,23 +49,30 @@ const OfferDetails = () => {
             </Typography>
           </Box>
         }
-        sx={styles.avatarContainer}
       />
       <CardContent sx={styles.textContainer}>
-        <Typography gutterBottom sx={styles.name}>
+        <Typography gutterBottom sx={{...styles.name, ...styles.line}}>
           Advanced Quantum Mechanics: Theoretical Concepts, Mathematical
           Formulations in Modern Physics
         </Typography>
-        <Stack direction='row' spacing={1} sx={styles.chipContainer}>
-          <Chip label='GERMAN' sx={styles.chip} />
-          <Chip
+        <Stack direction='row' sx={styles.chipContainer}>
+          <Box sx={styles.chipBox}>
+            <Typography sx={styles.chipTitle} variant='body2'>SUBJECT:</Typography>
+            <Chip label='GERMAN' sx={styles.chip} />
+          </Box>
+          <Box sx={styles.chipBox}>
+            <Typography sx={styles.chipTitle} variant='body2'>LEVEL:</Typography>
+            <Chip
             label='BEGINNER - ADVANCED'
             sx={{
               ...styles.chip,
               backgroundColor: 'rgba(121, 178, 96, 0.2)',
-              fontWeight: '400'
+              fontWeight: '400',
+              ml: '17px'
             }}
-          />
+            />
+          </Box>
+
         </Stack>
         <Typography paragraph sx={styles.description} variant='body2'>
           Hello. There are many variations of passages of Lorem Ipsum available,
@@ -73,11 +88,20 @@ const OfferDetails = () => {
         </Box>
       </CardContent>
       <Box sx={styles.priceContainer}>
-        <Typography sx={styles.price}>
-          75 UAH
-          <br />
-          <Typography sx={styles.priceHour}>/HOUR</Typography>
-        </Typography>
+        <Box sx={styles.ratingPhoneContainer}>
+          <Typography sx={styles.price}>
+            75 UAH
+            <br />
+            <Typography component="span" sx={styles.priceHour}>/HOUR</Typography>
+          </Typography>
+          <Box>
+            <Box sx={styles.starContainer}>
+              <StarIcon sx={{color: '#FFB000'}}/>
+              <Typography component="span" sx={styles.ratingText}>5.0</Typography>
+            </Box>
+            <Typography sx={styles.reviewPhoneText} variant='body2'>23 reviews</Typography>
+          </Box>
+        </Box>
         <BookmarkBorderIcon sx={styles.iconPosition} />
         <Box sx={styles.buttonsContainer}>
           <Button
