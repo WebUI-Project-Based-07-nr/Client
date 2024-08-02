b8504a738d73337166f27675cef6254f01062956
Roman Gordiienko <rom4uk691@gmail.com>
2024-08-02 14:34:16 +0300

    feature/165/find-offer-sort-menu

--- src/pages/find-offers/FindOffers.tsx
+++ src/pages/find-offers/FindOffers.tsx
@@ -1,7 +1,23 @@
+import { Box } from '@mui/material'
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import SortMenu from '~/components/sort-menu/SortMenu'
 
 const FindOffers = () => {
-  return <PageWrapper>Find offers</PageWrapper>
+  const items = ['Newest', 'Rating', 'Price low-high', 'Price high-low']
+  return (
+    <PageWrapper>
+      <Box
+        alignItems='center'
+        display='Flex'
+        flexDirection='row-reverse'
+        gap='5px'
+      >
+        <Box>
+          <SortMenu items={items} />
+        </Box>
+      </Box>
+    </PageWrapper>
+  )
 }
 
 export default FindOffers
