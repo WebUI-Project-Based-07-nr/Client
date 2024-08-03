b8edf23717af9f9c8777d2e4103bc4f9153a646e
marek777-lol <markgol000@gmail.com>
2024-08-03 20:33:06 +0200

    F

---
+++ src/components/user-steps-wrapper/UserStepsWrapper.styles.ts
@@ -0,0 +1,18 @@
+export const styles = {
+  modal: {
+    maxHeight: { sm: '652px' },
+    height: '100%',
+    maxWidth: '1130px',
+    width: '100%'
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 15,
+    right: 20,
+    color: 'black',
+    backgroundColor: 'white',
+    '&:hover': {
+      backgroundColor: 'transparent'
+    }
+  }
+}
