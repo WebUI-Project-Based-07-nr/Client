1045aaaf925886657b2f554f97035c8bd9e4ad0a
marek777-lol <markgol000@gmail.com>
2024-08-06 00:49:22 +0200

    done student faq

--- src/containers/student-home-page/faq/accordionItems.ts
+++ src/containers/student-home-page/faq/accordionItems.ts
@@ -1,18 +1,20 @@
+import studentHomePage from '~/constants/translations/en/student-home-page.json'
+
 export const accordionItems = [
   {
-    title: 'studentHomePage.faq.findTutor',
-    description: 'studentHomePage.faq.findTutorDescription'
+    title: studentHomePage.faq.findTutor,
+    description: studentHomePage.faq.findTutorDescription
   },
   {
-    title: 'studentHomePage.faq.bookLesson',
-    description: 'studentHomePage.faq.bookLessonDescription'
+    title: studentHomePage.faq.bookLesson,
+    description: studentHomePage.faq.bookLessonDescription
   },
   {
-    title: 'studentHomePage.faq.rules',
-    description: 'studentHomePage.faq.rulesDescription'
+    title: studentHomePage.faq.rules,
+    description: studentHomePage.faq.rulesDescription
   },
   {
-    title: 'studentHomePage.faq.howPayLessons',
-    description: 'studentHomePage.faq.howPayLessonsDescription'
+    title: studentHomePage.faq.howPayLessons,
+    description: studentHomePage.faq.howPayLessonsDescription
   }
 ]
