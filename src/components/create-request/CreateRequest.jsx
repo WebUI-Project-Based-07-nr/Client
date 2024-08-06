9a3a168a3aee7aa831e92ee50f4735e689d39b27
marek777-lol <markgol000@gmail.com>
2024-08-06 14:59:49 +0200

    created placeholder modal

--- src/components/create-request/CreateRequest.jsx
+++ src/components/create-request/CreateRequest.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { useAppSelector } from '~/hooks/use-redux'
+import { useModalContext } from '~/context/modal-context'
 
 import useBreakpoints from '~/hooks/use-breakpoints'
 import TitleBlock from '~/components/title-block/TitleBlock'
 import AppButton from '~/components/app-button/AppButton'
+import PlaceHolderPopUp from './PlaceHolderPopUp'
 
 import icon from '~/assets/img/find-offer/subject_icon.png'
 import { styles } from '~/components/create-request/create-request.styles'
@@ -13,6 +15,11 @@ const CreateRequest = ({ translationKey }) => {
   const { t } = useTranslation()
   const { isMobile } = useBreakpoints()
   const { userRole } = useAppSelector((state) => state.appMain)
+  const { openModal } = useModalContext()
+
+  const handleOpenModal = () => {
+    openModal({ component: <PlaceHolderPopUp /> })
+  }
 
   return (
     <TitleBlock
@@ -20,7 +27,11 @@ const CreateRequest = ({ translationKey }) => {
       style={styles.container}
       translationKey={translationKey}
     >
-      <AppButton component={Link} fullWidth={isMobile}>
+      <AppButton
+        component={Link}
+        fullWidth={isMobile}
+        onClick={handleOpenModal}
+      >
         {t(`${translationKey}.button.${userRole}`)}
       </AppButton>
     </TitleBlock>
