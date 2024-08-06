14e0805202cd37124be4ea6437dc7a60e0d83fe8
Yaroslav Bakalyna <bakalyna.yaroslav@gmail.com>
2024-08-06 16:41:56 +0300

    changed LessonsContainer

--- src/containers/my-resources/lesson-container/LessonsContainer.jsx
+++ src/containers/my-resources/lesson-container/LessonsContainer.jsx
@@ -1,18 +1,16 @@
 import AddResourceWithInput from '~/containers/my-resources/add-resource-with-input/AddResourceWithInput'
-import { useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 import { authRoutes } from '~/router/constants/authRoutes'
 
 const LessonContainer = () => {
   const { t } = useTranslation()
-  const searchTitle = useRef
 
   return (
     <AddResourceWithInput
       btnText={t('myResourcesPage.lessons.addBtn')}
       fetchData={() => {}}
       link={authRoutes.myResources.newLesson.path}
-      searchRef={searchTitle}
+      searchRef={{}}
     />
   )
 }
