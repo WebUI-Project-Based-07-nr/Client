33244d530256dbb46789fac787b2baafc91f7542
RomanKhorol <108153825+RomanKhorol@users.noreply.github.com>
2024-08-07 13:35:09 +0300

    feature/165/find-offer-sort-menu (#175)
    * feature/165/find-offer-sort-menu
    
    * Change initial state
    
    * Add readOnly for props required by sonar

--- src/pages/find-offers/FindOffers.tsx
+++ src/pages/find-offers/FindOffers.tsx
@@ -1,16 +1,19 @@
+
 import * as React from 'react'
+import SortMenu from '~/components/sort-menu/SortMenu'
 import OfferDetails from '~/pages/offer-details/OfferDetails'
-
 import ToggleButtons from '~/components/toggle-button/ToggleButtons'
-
+        
 const FindOffers = () => {
   const [alignment, setAlignment] = React.useState<string>('left')
+  const items = ['Newest', 'Rating', 'Price low-high', 'Price high-low']
   return (
     <>
+      <SortMenu items={items} />
       <ToggleButtons alignment={alignment} setAlignment={setAlignment} />
       <OfferDetails />
-    </>
-  )
+    </>   
+)
 }
 
 export default FindOffers
