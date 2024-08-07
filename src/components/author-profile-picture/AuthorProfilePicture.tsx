c26332df395f79801b8be1d2b016915eb7087c3f
marek777-lol <markgol000@gmail.com>
2024-08-07 12:42:15 +0200

    testing placeholder image

--- src/components/author-profile-picture/AuthorProfilePicture.tsx
+++ src/components/author-profile-picture/AuthorProfilePicture.tsx
@@ -1,20 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { styles } from '~/components/author-profile-picture/AuthorProfilePicture.styles'
 import { Avatar } from '@mui/material'
-// import { FC, useCallback } from 'react'
-import { FC } from 'react'
+import { FC, useCallback } from 'react'
 import { useNavigate } from 'react-router'
 
-// import { userService } from '~/services/user-service'
-// import {
-//   ErrorResponse,
-//   GetUsersParams,
-//   UserResponse,
-//   UserRoleEnum
-// } from '~/types'
+import { userService } from '~/services/user-service'
+import {
+  ErrorResponse,
+  GetUsersParams,
+  UserResponse,
+  UserRoleEnum
+} from '~/types'
 import { useSnackBarContext } from '~/context/snackbar-context'
-// import { defaultResponses, snackbarVariants } from '~/constants'
-// import useAxios from '~/hooks/use-axios'
+import { defaultResponses, snackbarVariants } from '~/constants'
+import useAxios from '~/hooks/use-axios'
 
 interface ProfileAvatar {
   authorId: string
@@ -28,30 +27,34 @@ const AuthorProfilePicture: FC<ProfileAvatar> = ({ authorId }) => {
     navigate('/')
   }
 
-  // const onResponseError = useCallback(
-  //   (error: ErrorResponse) => {
-  //     setAlert({
-  //       severity: snackbarVariants.error,
-  //       message: error ? `errors.${error.code}` : ''
-  //     })
-  //   },
-  //   [setAlert]
-  // )
-  // const getOfferService = useCallback(() => {
-  //   return userService.getUserById(authorId, UserRoleEnum.Admin)
-  // }, [authorId])
+  const onResponseError = useCallback(
+    (error: ErrorResponse) => {
+      setAlert({
+        severity: snackbarVariants.error,
+        message: error ? `errors.${error.code}` : ''
+      })
+    },
+    [setAlert]
+  )
+  const getOfferService = useCallback(() => {
+    return userService.getUserById(authorId, UserRoleEnum.Admin)
+  }, [authorId])
 
-  // const { response, loading } = useAxios<UserResponse, GetUsersParams>({
-  //   service: getOfferService,
-  //   defaultResponse: defaultResponses.object,
-  //   onResponseError
-  // })
+  const { response, loading } = useAxios<UserResponse, GetUsersParams>({
+    service: getOfferService,
+    defaultResponse: defaultResponses.object,
+    onResponseError
+  })
 
   return (
     <Avatar
       alt='User'
       onClick={openProfile}
-      // src={!loading ? response.photo : ''}
+      src={
+        !loading
+          ? response.photo
+          : 'https://cdn.vectorstock.com/i/500p/08/19/gray-photo-placeholder-icon-design-ui-vector-35850819.jpg'
+      }
       sx={styles.avatarImg}
     />
   )
