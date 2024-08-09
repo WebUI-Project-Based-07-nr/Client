93d30b077299a4b51685e3d72215712919844933
marek777-lol <markgol000@gmail.com>
2024-08-09 23:23:52 +0200

    done

--- src/pages/find-offers/FindOffers.tsx
+++ src/pages/find-offers/FindOffers.tsx
@@ -3,13 +3,16 @@ import { translationKey } from '~/components/create-request/create-request-stude
 import * as React from 'react'
 import OfferCards from '~/containers/offer-cards/OfferCards'
 import ToggleButtons from '~/components/toggle-button/ToggleButtons'
+import SortMenu from '~/components/sort-menu/SortMenu'
 
 const FindOffers = () => {
   const [alignment, setAlignment] = React.useState<string>('left')
+  const items = ['Newest', 'Rating', 'Price low-high', 'Price high-low']
+
   return (
     <>
-      <SortMenu items={items} />
       <CreateRequest translationKey={translationKey} />
+      <SortMenu items={items} />
       <ToggleButtons alignment={alignment} setAlignment={setAlignment} />
       <OfferCards isSquare={alignment !== 'left'} />
     </>
