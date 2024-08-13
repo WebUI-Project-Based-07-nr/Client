f1176ff1aebade9131d34eb888bbc4891826cf78
markgol777 <66869816+markgol777@users.noreply.github.com>
2024-08-13 08:26:35 +0200

    added how it work (#207)
    Co-authored-by: marek777-lol <markgol000@gmail.com>

---
+++ src/containers/student-home-page/student-how-it-works/howItWorks.style.js
@@ -0,0 +1,23 @@
+export const styles = {
+  root: {
+    width: '229px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: '20px',
+    fontWeight: 500,
+    mt: '24px',
+    fontFamily: 'Rubik'
+  },
+  description: {
+    textAlign: 'center',
+    mt: '16px',
+    fontFamily: 'Rubik'
+  },
+  img: {
+    width: '64px',
+    height: '64px'
+  }
+}
