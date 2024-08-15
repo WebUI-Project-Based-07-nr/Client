a39e8df01b371847cbb81392f1c31531222e2db5
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2024-08-15 15:52:49 +0300

    Implemented Link from react-router-dom for button redirect to All Offers (authRoutes.findOffers)

--- src/containers/student-home-page/student-how-it-works/how-it-works/HowItWorks.jsx
+++ src/containers/student-home-page/student-how-it-works/how-it-works/HowItWorks.jsx
@@ -9,40 +9,48 @@ import TitleWithDescription from '~/components/title-with-description/TitleWithD
 import AppButton from '~/components/app-button/AppButton'
 import { student } from '~/constants'
 import HowItWorksCard from '~/containers/student-home-page/student-how-it-works/how-it-works-card/HowItWorksCard'
+import { studentRoutes } from '~/router/constants/studentRoutes'
+import { useNavigate } from 'react-router-dom'
+import { authRoutes } from '~/router/constants/authRoutes'
 
 const HowItWorks = ({ userRole }) => {
   const { t } = useTranslation()
+  const navigate = useNavigate()
 
-  const button =
-    userRole === student ? (
-      <AppButton>{t('studentHomePage.findTutorBlock.button')}</AppButton>
-    ) : (
-      <AppButton>{t('tutorHomePage.findStudentBlock.button')}</AppButton>
-    )
+  const handleFindOffers = () => {
+    navigate(authRoutes.findOffers.path)
+  }
 
-  const arrayWithCards =
+  const cardsData =
     userRole === student ? howItWorksStudentCards : howItWorksTutorCards
 
   return (
-    <Box sx={styles.container}>
-      <TitleWithDescription
-        description={t('studentHomePage.howItWorks.description')}
-        style={styles.titleWithDescription}
-        title={t('studentHomePage.howItWorks.title')}
-      />
-      <Box sx={styles.cardsContainer}>
-        {arrayWithCards.map((item) => (
-          <HowItWorksCard
-            description={t(item.description)}
-            img={t(item.image)}
-            key={item.title}
-            title={t(item.title)}
-          />
-        ))}
+    <Box id={studentRoutes.navBar.howItWorks.route}>
+      <Box sx={styles.container}>
+        <TitleWithDescription
+          description={t('studentHomePage.howItWorks.description')}
+          style={styles.titleWithDescription}
+          title={t('studentHomePage.howItWorks.title')}
+        />
+        <Box sx={styles.cardsContainer}>
+          {cardsData.map((item) => (
+            <HowItWorksCard
+              description={t(item.description)}
+              img={item.image}
+              key={item.title}
+              title={t(item.title)}
+            />
+          ))}
+        </Box>
+        <AppButton onClick={handleFindOffers}>
+          {t(
+            userRole === student
+              ? 'studentHomePage.findTutorBlock.button'
+              : 'tutorHomePage.findStudentBlock.button'
+          )}
+        </AppButton>
       </Box>
-      <Box>{button}</Box>
     </Box>
   )
 }
-
 export default HowItWorks
